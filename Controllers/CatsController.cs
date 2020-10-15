8c5be24023768a9ecd62fbc3106a19c2017cea67
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-15 14:49:58 -0600

    set up cat delete maybe

--- Controllers/CatsController.cs
+++ Controllers/CatsController.cs
@@ -10,52 +10,65 @@ namespace catlady.Controllers
   [Route("api/[controller]")]
   public class CatsController : ControllerBase
   {
-        private readonly CatsService _catsService;
-        public CatsController(CatsService catsService)
-        {
-            _catsService = catsService;
-        }
+    private readonly CatsService _catsService;
+    public CatsController(CatsService catsService)
+    {
+      _catsService = catsService;
+    }
 
 
 
-        [HttpGet]
-        public ActionResult<IEnumerable<Cat>> Get()
-        {
-            try
-            {
-                return Ok(_catsService.GetCats());
-            }
-            catch (Exception err)
-            {
-                return BadRequest(err.Message);
-            }
-        }
+    [HttpGet]
+    public ActionResult<IEnumerable<Cat>> Get()
+    {
+      try
+      {
+        return Ok(_catsService.GetCats());
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
 
-        [HttpPost]
-        public ActionResult<Cat> Create([FromBody] Cat newCat)
-        {
-            try
-            {
-                return Ok(_catsService.Create(newCat));
-            }
-            catch (System.Exception err)
-            {
-                return BadRequest(err.Message);
-            }
-        }
-    
-        [HttpGet("{catId}")]
-         public ActionResult<Cat> GetById(string catId)
-        {
-            try
-            {
-                return Ok(_catsService.GetCatById(catId));
-            }
-            catch (Exception err)
-            {
-                return BadRequest(err.Message);
-            }
-        }
+    [HttpPost]
+    public ActionResult<Cat> Create([FromBody] Cat newCat)
+    {
+      try
+      {
+        return Ok(_catsService.Create(newCat));
+      }
+      catch (System.Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpGet("{catId}")]
+    public ActionResult<Cat> GetById(string catId)
+    {
+      try
+      {
+        return Ok(_catsService.GetCatById(catId));
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
+
+    [HttpDelete("{catId}")]
+    public ActionResult<IEnumerable<Cat>> Delete(string catId)
+    {
+      try
+      {
+        return Ok(_catsService.DeleteCats(catId));
+      }
+      catch (Exception err)
+      {
+        return BadRequest(err.Message);
+      }
+    }
 
 
   }
--- Services/CatsService.cs
+++ Services/CatsService.cs
@@ -9,23 +9,36 @@ namespace catlady.Services
   {
     public IEnumerable<Cat> GetCats()
     {
-        return FAKEDB.Cats;
+      return FAKEDB.Cats;
     }
 
     public Cat Create(Cat newCat)
     {
-        FAKEDB.Cats.Add(newCat);
-        return newCat;
+      FAKEDB.Cats.Add(newCat);
+      return newCat;
     }
 
     public Cat GetCatById(string id)
     {
-          Cat foundCat =  FAKEDB.Cats.Find(c => c.Id == id);
-          if(foundCat != null)
-          {
-          return foundCat;
-          }
-        throw new Exception("No cat like that round these parts, partna");
+      Cat foundCat = FAKEDB.Cats.Find(c => c.Id == id);
+      if (foundCat != null)
+      {
+        return foundCat;
+      }
+      throw new Exception("No cat like that round these parts, partna");
+    }
+
+    public Cat DeleteCats(string catId)
+    {
+      int index = FAKEDB.Cats.FindIndex(c => c.Id == catId)
+      if (index != 0)
+      {
+        FAKEDB.Cats.RemoveAt(index)
+
+
+      }
+      throw new Exception("No cat like that round these parts, partna");
+
     }
   }
 }
